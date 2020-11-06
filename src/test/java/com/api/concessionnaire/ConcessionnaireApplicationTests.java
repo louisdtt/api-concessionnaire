41178255741bcee51371784035ad35ba23b648a7
Louis <louis.dutoitld@gmail.com>
2020-11-06 20:58:27 +0100

    Init project

---
+++ src/test/java/com/api/concessionnaire/ConcessionnaireApplicationTests.java
@@ -0,0 +1,13 @@
+package com.api.concessionnaire;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConcessionnaireApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
