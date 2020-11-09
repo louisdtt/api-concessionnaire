dfe83717db822070402ea415feecf937f6ecf5f8
Louis <louis.dutoitld@gmail.com>
2020-11-09 14:06:18 +0100

    :sparkles: Add Concessionnaire API

--- src/main/java/com/api/concessionnaire/model/Concessionnaire.java
+++ src/main/java/com/api/concessionnaire/model/Concessionnaire.java
@@ -21,4 +21,36 @@ public class Concessionnaire {
             inverseJoinColumns = @JoinColumn(name = "concessionnaire_id")
     )
     Set<Marque> marques;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Adresse getAdresse() {
+        return adresse;
+    }
+
+    public void setAdresse(Adresse adresse) {
+        this.adresse = adresse;
+    }
+
+    public Set<Marque> getMarques() {
+        return marques;
+    }
+
+    public void setMarques(Set<Marque> marques) {
+        this.marques = marques;
+    }
 }
