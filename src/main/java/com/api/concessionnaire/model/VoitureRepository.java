09942dc89169092d52587bd4aa2b68292db9e78c
Louis <louis.dutoitld@gmail.com>
2020-11-09 14:57:00 +0100

    :sparkles: Add Voiture API

--- src/main/java/com/api/concessionnaire/model/VoitureRepository.java
+++ src/main/java/com/api/concessionnaire/model/VoitureRepository.java
@@ -3,6 +3,5 @@ package com.api.concessionnaire.model;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
-@RepositoryRestResource(path = "voiture")
 public interface VoitureRepository extends JpaRepository<Voiture, Integer> {
 }
