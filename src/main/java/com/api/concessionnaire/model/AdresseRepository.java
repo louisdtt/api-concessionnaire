c483242e588a69cf20750f4fff6e91b8fe1a3576
Pierre FONTAINE <pierrefontaine00@gmail.com>
2020-11-09 15:29:57 +0100

    :bug: Bug fix.

--- src/main/java/com/api/concessionnaire/model/AdresseRepository.java
+++ src/main/java/com/api/concessionnaire/model/AdresseRepository.java
@@ -1,7 +1,6 @@
 package com.api.concessionnaire.model;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
 public interface AdresseRepository extends JpaRepository<Adresse, Integer> {
 }
